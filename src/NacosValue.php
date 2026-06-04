2b3f750da95714bdcd68bceb58b8f49c973fc33b
原点 <467490186@qq.com>
2026-06-04 10:35:49 +0800

    feat：增加获取配置类静态方法

--- src/NacosValue.php
+++ src/NacosValue.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace yuandian\WebmanNacos;
 
+use yuandian\Container\Container;
+
 /**
  * Utility class for directly reading Nacos config values.
  *
@@ -22,5 +24,18 @@ class NacosValue
     public static function get(string $key, mixed $default = null, string $configId = 'default'): mixed
     {
         return NacosConfigBootstrap::getCachedConfig($key, $default, $configId);
+    }
+
+    /**
+     * 通过配置类获取配置
+     * @template T of object
+     * @param class-string<T> $className
+     * @return T
+     * @date 2026/6/4 上午10:33
+     * @author 原点 467490186@qq.com
+     */
+    public static function getConfigClass(string $className): object
+    {
+        return Container::getInstance()->make($className);
     }
 }
